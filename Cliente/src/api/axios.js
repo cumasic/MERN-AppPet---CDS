fcc7b727cd2bd65a65663183eeb3ac4ed45fcf5c
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2023-12-11 16:04:29 -0500

    Se agrego api

--- Cliente/src/api/axios.js
+++ Cliente/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create ({
-    baseURL: 'https://backend-zwqf.onrender.com',
+    baseURL: 'https://backend-zwqf.onrender.com/api',
     withCredentials: true
 })
 
